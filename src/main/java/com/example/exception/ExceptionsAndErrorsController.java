1e61ed5e4480e102fc9db9061b154966a3195285
dysh 99 <ddms2001@gmail.com>
2025-06-22 16:32:53 +0000

    deleted exception

--- src/main/java/com/example/exception/ExceptionsAndErrorsController.java
+++ src/main/java/com/example/exception/ExceptionsAndErrorsController.java
@@ -1,7 +1,6 @@
 package com.example.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,12 +8,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionsAndErrorsController {
-    @ExceptionHandler(MissingServletRequestParameterException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public @ResponseBody String handleMissingParams(MissingServletRequestParameterException ex){
-        return ex.getParameterName() + " is missing in the query paramiters";
-    }
-
     @ExceptionHandler(DuplicateUsernameException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     public @ResponseBody void handleDubplicateUsernames(){
